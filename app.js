3f9c9fc2f9263fad11e64f48f06316a32870bbe7
Hanna Bennett <bennetthanna22@gmail.com>
2019-03-10 20:35:06 -0700

    Long poll receive SQS messages, console log, and delete them

---
+++ app.js
@@ -0,0 +1,44 @@
+const AWS = require('aws-sdk');
+const express = require('express');
+
+const app = express();
+
+const sqs = new AWS.SQS({ region: 'us-east-1' });
+
+const SQS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/171578128461/reporting';
+
+const params = {
+    AttributeNames: [
+        "All"
+    ],
+    MaxNumberOfMessages: 1,
+    MessageAttributeNames: [
+        "All"
+    ],
+    QueueUrl: SQS_QUEUE_URL,
+    VisibilityTimeout: 20,
+    WaitTimeSeconds: 20
+};
+
+function receiveMessages(parameters) {
+    return sqs.receiveMessage(params).promise()
+        .then(res => {
+            if (res.Messages) {
+                console.log('Received message(s)');
+                console.log(res.Messages);
+                const deleteParams = {
+                  QueueUrl: SQS_QUEUE_URL,
+                  ReceiptHandle: res.Messages[0].ReceiptHandle
+                };
+                return sqs.deleteMessage(deleteParams).promise();
+            }
+            return Promise.resolve();
+        })
+        .then(res => receiveMessages(params))
+        .catch(err => {
+            console.log(`\nError: ${err}\n`);
+            return receiveMessages(params);
+        });
+}
+
+receiveMessages(params);
